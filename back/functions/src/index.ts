f3024bb4baedbb983c57979ae4318104474477de
Daniella Arteaga <darteag@bancolombia.com.co>
2024-11-08 17:41:26 -0500

    refactor(front): Update Firebase configuration in backend and rename Flutter project

--- back/functions/src/index.ts
+++ back/functions/src/index.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBVKECxc5lOkJLBl_Siv7MjBY7TiwFMeu0",
-  authDomain: "tasks-app-b53c1.firebaseapp.com",
-  projectId: "tasks-app-b53c1",
-  storageBucket: "tasks-app-b53c1.appspot.com",
-  messagingSenderId: "837347397183",
-  appId: "1:837347397183:web:7459af4a625032b543b28a",
-  measurementId: "G-TYQ8Z7H17C",
+  apiKey: "AIzaSyAN-0YySo2sbt_YGTmxGP6cdUFT0Me9MAA",
+  authDomain: "safety-app-ff42c.firebaseapp.com",
+  projectId: "safety-app-ff42c",
+  storageBucket: "safety-app-ff42c.firebasestorage.app",
+  messagingSenderId: "1091468216825",
+  appId: "1:1091468216825:web:eaafc18da78dc7be699528"
 };
 
 initializeApp(firebaseConfig);
