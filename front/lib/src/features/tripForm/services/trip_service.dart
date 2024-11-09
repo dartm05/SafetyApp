fcd638392bbe55a30e9785ac017bb3df038fdc4f
Daniella Arteaga <darteag@bancolombia.com.co>
2024-11-09 02:39:03 -0500

    refactor(trip_service): update TripService with additional methods and error handling

--- front/lib/src/features/tripForm/services/trip_service.dart
+++ front/lib/src/features/tripForm/services/trip_service.dart
@@ -1,18 +1,28 @@
 import 'dart:convert';
 import 'package:safety_app/src/core/providers/error_provider.dart';
+import 'package:safety_app/src/core/services/http_service.dart';
 
+import '../../../core/models/modal_model.dart';
 import '../models/trip.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class TripService {
   final ErrorProvider errorProvider;
+  final HttpService httpService;
+  final String baseUrlPlaces =
+      'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=';
+  final String api_key = dotenv.env['PLACES_API_KEY'] ?? '';
 
-  TripService({required this.errorProvider});
+  TripService({
+    required this.errorProvider,
+    required this.httpService,
+  });
 
   Future<List<String>> fetchPlaces(String place) async {
     final response = await http.get(
       Uri.parse(
-        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${place}&key=AIzaSyBGzIzmMiVwWqVr7-dK6-4dzxWZgZOS7Z8',
+        '$baseUrlPlaces$place&key=$api_key',
       ),
     );
 
@@ -28,7 +38,7 @@ class TripService {
   Future<List<String>> fetchTripPlaces(String place) async {
     final response = await http.get(
       Uri.parse(
-        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${place}&type=lodging&key=AIzaSyBGzIzmMiVwWqVr7-dK6-4dzxWZgZOS7Z8',
+        '$baseUrlPlaces$place&type=lodging&key=$api_key',
       ),
     );
     if (response.statusCode == 200) {
@@ -40,15 +50,109 @@ class TripService {
     }
   }
 
-  Future<void> createTrip(Trip trip) async {
-    // Create a new trip
+  Future<Trip?> getTrip(String userId, String tripId) async {
+    try {
+      var response = await httpService.get('/$userId/trips/$tripId');
+      return Trip.fromJson(jsonDecode(response.body));
+    } catch (error) {
+      errorProvider.showError(
+        error: Modal(
+          title: 'Error',
+          message: 'An error occurred while fetching trip. Please try again.',
+          actionText: 'Close',
+          action: () {
+            errorProvider.hideError();
+          },
+        ),
+      );
+      return null;
+    }
   }
 
-  Future<void> updateTrip(Trip trip) async {
-    // Update an existing trip
+  Future<Trip?> createTrip(String userId, Trip trip) async {
+    try {
+      var response =
+          await httpService.post('/$userId/trips', body: trip.toJson());
+      if (response.statusCode == 200) {
+        return Trip.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to create trip');
+      }
+    } catch (error) {
+      errorProvider.showError(
+        error: Modal(
+          title: 'Error',
+          message: 'An error occurred while creating trip. Please try again.',
+          actionText: 'Close',
+          action: () {
+            errorProvider.hideError();
+          },
+        ),
+      );
+    }
+    return null;
   }
 
-  Future<void> deleteTrip(Trip trip) async {
-    // Delete a trip
+  Future<Trip?> updateTrip(String userId, String tripId, Trip trip) async {
+    try {
+      var response =
+          await httpService.put('/$userId/trips/$tripId', body: trip.toJson());
+
+      if (response.statusCode > 300) {
+        throw Exception('Failed to update trip');
+      }
+      errorProvider.showError(
+        error: Modal(
+          title: 'Success',
+          message: 'Trip updated successfully',
+          actionText: 'Close',
+          action: () {
+            errorProvider.hideError();
+          },
+        ),
+      );
+    } catch (error) {
+      errorProvider.showError(
+        error: Modal(
+          title: 'Error',
+          message: 'An error occurred while updating trip. Please try again.',
+          actionText: 'Close',
+          action: () {
+            errorProvider.hideError();
+          },
+        ),
+      );
+    }
+    return null;
+  }
+
+  Future<void> deleteTrip(String userId, String tripId) async {
+    try {
+      var response = await httpService.delete('/trips/$tripId');
+      if (response.statusCode > 300) {
+        throw Exception('Failed to delete trip');
+      }
+      errorProvider.showError(
+        error: Modal(
+          title: 'Success',
+          message: 'Trip deleted successfully',
+          actionText: 'Close',
+          action: () {
+            errorProvider.hideError();
+          },
+        ),
+      );
+    } catch (error) {
+      errorProvider.showError(
+        error: Modal(
+          title: 'Error',
+          message: 'An error occurred while deleting trip. Please try again.',
+          actionText: 'Close',
+          action: () {
+            errorProvider.hideError();
+          },
+        ),
+      );
+    }
   }
 }
