01d7e99886631240c8577c9abf294b43f5b13536
Daniella Arteaga <darteag@bancolombia.com.co>
2024-11-09 02:39:13 -0500

    feat(trip): add TripUseCases class with methods for fetching, creating, updating, and deleting trips

---
+++ front/lib/src/features/tripForm/usecases/trip_usecases.dart
@@ -0,0 +1,34 @@
+
+import '../models/trip.dart';
+import '../services/trip_service.dart';
+
+class TripUseCases {
+ final TripService tripService;
+
+  TripUseCases({required this.tripService});
+
+  Future<List<String>> fetchPlaces(String place) async {
+    return await tripService.fetchPlaces(place);
+  }
+
+  Future<List<String>> fetchTripPlaces(String place) async {
+    return await tripService.fetchTripPlaces(place);
+  }
+
+  Future<Trip?> getTrip(String userId, String tripId) async {
+    return await tripService.getTrip(userId, tripId);
+  }
+
+  Future<Trip?> createTrip(String userId, Trip trip) async {
+    return await tripService.createTrip(userId, trip);
+  }
+
+  Future<Trip?> updateTrip(String userId, String tripId, Trip trip) async {
+    return await tripService.updateTrip(userId, tripId, trip);
+  }
+
+  Future<void> deleteTrip(String userId, String tripId) async {
+    return await tripService.deleteTrip(userId, tripId);
+  }
+
+}
