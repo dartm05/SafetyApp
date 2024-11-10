8d7c7c7a4fcc3c806f783068b00c7d1398e2c704
Daniella Arteaga <darteag@bancolombia.com.co>
2024-11-10 02:12:46 -0500

    feat(trip_card): Add TripCard widget to display trip details

---
+++ front/lib/src/features/trip_list/widgets/trip_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:safety_app/src/data/models/trip.dart';
+
+class TripCard extends StatelessWidget {
+  final Trip trip;
+  const TripCard({super.key, required this.trip});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.all(16),
+      constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
+      decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(8),
+      
+      ),
+      child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+        'Trip to ${trip.destination.split(",").first.split(" ").take(2).join(" ")}',
+        style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        const SizedBox(height: 8),
+        Text(
+        trip.destination,
+        style: Theme.of(context).textTheme.labelMedium,
+        ),
+        const SizedBox(height: 8),
+        Text(
+        DateFormat('MMMM dd, yyyy').format(trip.startDate),
+        style: Theme.of(context).textTheme.bodyMedium,
+        ),
+      ],
+      ),
+    );
+  }
+}
