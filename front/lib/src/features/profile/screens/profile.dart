dbc9b858351cfadbae16dfccfe16bf7f5a71b395
Daniella Arteaga <darteag@bancolombia.com.co>
2024-11-12 15:28:44 -0500

    fix(profile): Update padding and width limits on web

--- front/lib/src/features/profile/screens/profile.dart
+++ front/lib/src/features/profile/screens/profile.dart
@@ -82,7 +82,12 @@ class _ProfileFormState extends State<ProfileForm> {
 
   @override
   Widget build(BuildContext context) {
-    const sizedBox = SizedBox(height: 20);
+    double width = MediaQuery.of(context).size.width;
+    SizedBox sizedBox = width > 800
+        ? SizedBox(
+            height: 60,
+          )
+        : SizedBox(height: 20);
     const sizedboxM = SizedBox(height: 10);
     final profileProvider = Provider.of<ProfileProvider>(context, listen: true);
     return !profileProvider.isLoading
@@ -100,138 +105,141 @@ class _ProfileFormState extends State<ProfileForm> {
                   ),
                 ),
                 Center(
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: [
-                      sizedBox,
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                        child: Container(
-                          constraints: const BoxConstraints(maxWidth: 600),
-                          child: Form(
-                            key: _formKey,
-                            child: ListView(
-                              shrinkWrap: true,
-                              children: [
-                                DropdownButtonFormField<String>(
-                                  key: const Key('gender'),
-                                  decoration: const InputDecoration(
-                                      labelText: 'Gender'),
-                                  value: _gender,
-                                  items: _genderOptions
-                                      .map((gender) => DropdownMenuItem(
-                                            value: gender,
-                                            child: Text(gender),
-                                          ))
-                                      .toList(),
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _gender = value!;
-                                    });
-                                  },
-                                ),
-                                sizedBox,
-                                TextFormField(
-                                  decoration:
-                                      const InputDecoration(labelText: 'Age'),
-                                  keyboardType: TextInputType.number,
-                                  initialValue: _age,
-                                  onSaved: (value) {
-                                    _age = value!;
-                                  },
-                                  validator: (value) => value!.isEmpty
-                                      ? 'Please enter your age'
-                                      : null,
-                                ),
-                                sizedBox,
-                                DropdownButtonFormField<String>(
-                                  key: const Key('ethnicity'),
-                                  decoration: const InputDecoration(
-                                      labelText: 'Ethnicity'),
-                                  value: _ethnicity,
-                                  items: _ethnicityOptions
-                                      .map((ethnic) => DropdownMenuItem(
-                                            value: ethnic,
-                                            child: Text(ethnic),
-                                          ))
-                                      .toList(),
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _ethnicity = value!;
-                                    });
-                                  },
-                                  validator: (value) => value!.isEmpty
-                                      ? 'Please enter your ethnicity'
-                                      : null,
-                                ),
-                                sizedBox,
-                                SwitchListTile(
-                                  title: const Text('Disability'),
-                                  value: _disability,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _disability = value;
-                                    });
-                                  },
-                                ),
-                                sizedboxM,
-                                SwitchListTile(
-                                  title: const Text('Traveling with Children'),
-                                  value: _travelingWithChildren,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _travelingWithChildren = value;
-                                    });
-                                  },
-                                ),
-                                sizedBox,
-                                DropdownButtonFormField<String>(
-                                  key: const Key('preferredModeOfTransport'),
-                                  decoration: const InputDecoration(
-                                      labelText:
-                                          'Preferred Mode of Transportation'),
-                                  value: _preferredModeOfTransport,
-                                  items: _transportOptions
-                                      .map((transport) => DropdownMenuItem(
-                                            value: transport,
-                                            child: Text(transport),
-                                          ))
-                                      .toList(),
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _preferredModeOfTransport = value!;
-                                    });
-                                  },
-                                ),
-                                sizedBox,
-                                DropdownButtonFormField<String>(
-                                  key: const Key('dataUsage'),
-                                  decoration: const InputDecoration(
-                                      labelText: 'Data Usage'),
-                                  value: _dataUsage,
-                                  items: _dataUsageOptions
-                                      .map((usage) => DropdownMenuItem(
-                                            value: usage,
-                                            child: Text(usage),
-                                          ))
-                                      .toList(),
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _dataUsage = value!;
-                                    });
-                                  },
-                                ),
-                                sizedBox,
-                                ElevatedButton(
-                                  onPressed: () => _saveProfile(context),
-                                  child: const Text('Save Profile'),
-                                ),
-                              ],
+                  child: SizedBox(
+                    width: width > 800 ? 700 : width,
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        sizedBox,
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                          child: Container(
+                            constraints: const BoxConstraints(maxWidth: 600),
+                            child: Form(
+                              key: _formKey,
+                              child: ListView(
+                                shrinkWrap: true,
+                                children: [
+                                  DropdownButtonFormField<String>(
+                                    key: const Key('gender'),
+                                    decoration: const InputDecoration(
+                                        labelText: 'Gender'),
+                                    value: _gender,
+                                    items: _genderOptions
+                                        .map((gender) => DropdownMenuItem(
+                                              value: gender,
+                                              child: Text(gender),
+                                            ))
+                                        .toList(),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _gender = value!;
+                                      });
+                                    },
+                                  ),
+                                  sizedBox,
+                                  TextFormField(
+                                    decoration:
+                                        const InputDecoration(labelText: 'Age'),
+                                    keyboardType: TextInputType.number,
+                                    initialValue: _age,
+                                    onSaved: (value) {
+                                      _age = value!;
+                                    },
+                                    validator: (value) => value!.isEmpty
+                                        ? 'Please enter your age'
+                                        : null,
+                                  ),
+                                  sizedBox,
+                                  DropdownButtonFormField<String>(
+                                    key: const Key('ethnicity'),
+                                    decoration: const InputDecoration(
+                                        labelText: 'Ethnicity'),
+                                    value: _ethnicity,
+                                    items: _ethnicityOptions
+                                        .map((ethnic) => DropdownMenuItem(
+                                              value: ethnic,
+                                              child: Text(ethnic),
+                                            ))
+                                        .toList(),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _ethnicity = value!;
+                                      });
+                                    },
+                                    validator: (value) => value!.isEmpty
+                                        ? 'Please enter your ethnicity'
+                                        : null,
+                                  ),
+                                  sizedBox,
+                                  SwitchListTile(
+                                    title: const Text('Disability'),
+                                    value: _disability,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _disability = value;
+                                      });
+                                    },
+                                  ),
+                                  sizedboxM,
+                                  SwitchListTile(
+                                    title: const Text('Traveling with Children'),
+                                    value: _travelingWithChildren,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _travelingWithChildren = value;
+                                      });
+                                    },
+                                  ),
+                                  sizedBox,
+                                  DropdownButtonFormField<String>(
+                                    key: const Key('preferredModeOfTransport'),
+                                    decoration: const InputDecoration(
+                                        labelText:
+                                            'Preferred Mode of Transportation'),
+                                    value: _preferredModeOfTransport,
+                                    items: _transportOptions
+                                        .map((transport) => DropdownMenuItem(
+                                              value: transport,
+                                              child: Text(transport),
+                                            ))
+                                        .toList(),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _preferredModeOfTransport = value!;
+                                      });
+                                    },
+                                  ),
+                                  sizedBox,
+                                  DropdownButtonFormField<String>(
+                                    key: const Key('dataUsage'),
+                                    decoration: const InputDecoration(
+                                        labelText: 'Data Usage'),
+                                    value: _dataUsage,
+                                    items: _dataUsageOptions
+                                        .map((usage) => DropdownMenuItem(
+                                              value: usage,
+                                              child: Text(usage),
+                                            ))
+                                        .toList(),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _dataUsage = value!;
+                                      });
+                                    },
+                                  ),
+                                  sizedBox,
+                                  ElevatedButton(
+                                    onPressed: () => _saveProfile(context),
+                                    child: const Text('Save Profile'),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ],
